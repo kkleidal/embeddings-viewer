bd2b9f717956356d64ec14529b63ea45a4a5710f
Ken Leidal <kkleidal@csail.mit.edu>
2017-12-03 19:54:43 -0500

    added control for coloring/shape

--- convert.py
+++ convert.py
@@ -13,8 +13,10 @@ def stringify_extra(extra, link_prefix=None):
 def get_color(x, cmap="Paired"):
     cmap = plt.get_cmap("Paired")
     color = "#" + "".join("%02x" % int(255 * x) for x in cmap(x)[:-1])
-    print(color)
     return color
+
+def get_shape(x):
+    return ["circle", "square", "triangle", "cross"][x % 4]
 
 def convert(meta_file_link, link_prefix=None):
     with open(meta_file_link, "r") as f:
@@ -22,13 +24,25 @@ def convert(meta_file_link, link_prefix=None):
     assert meta.get("embeddings-viewer-version", "") == 1
     chart = {
         "animationEnabled": True,
+        "axisY": {
+            "gridThickness": 0
+        },
+        "axisX": {},
         "height": 800,
         "width": 800,
     }
     if "title" in meta:
-        chart["title"] = {"text": meta["title"]}
-    color_options = dict()
-    shape_options = dict()
+        chart["title"] = {
+                "text": meta["title"],
+                "fontSize": 16
+            }
+    if "subtitle" in meta:
+        chart["subtitles"] = [{
+                "text": meta["subtitle"],
+                "fontSize": 14
+            }]
+    color_options = OrderedDict()
+    shape_options = OrderedDict()
     extras = OrderedDict()
     fields = set()
     dps = []
@@ -51,36 +65,44 @@ def convert(meta_file_link, link_prefix=None):
                 if value not in color_options[name]:
                     color_options[name][value] = []
                 color_options[name][value].append(i)
+        x = data["x"]
+        y = data["y"]
         dp = {
-            "x": data["x"],
-            "y": data["y"],
+            "x": x,
+            "y": y,
         }
         tt = "<br/>".join((stringify_extra(extra, link_prefix=link_prefix) for extra in data.get("extras", [])))
-        #for extra in data.get("extras", []):
-        #    extras[extra["id"]] = {
-        #        "type": extra["type"],
-        #        "id": extra["id"],
-        #        "name": extra["name"]
-
-        #    }
-        #    dp[extra["id"]] = extra["value"]
         dp["toolTipContent"] = tt
         dps.append(dp)
-    # tt = "<br/>".join((stringify_extra(extra, link_prefix=link_prefix) for extra in extras.values()))
-    color_option = sorted(color_options.keys())[0]
-    print(color_option)
-    vals = sorted(color_options[color_option])
-    val_map = {val: i for i, val in enumerate(vals)}
-    all_ds = []
-    for i, val in enumerate(vals):
-        ds = {
-            "type": "scatter",
-            "markerColor": get_color(val),
-            "dataPoints": [dp for i, dp in enumerate(dps) if i in set(color_options[color_option][val])],
-        }
-        all_ds.append(ds)
-    chart["data"] = all_ds
-    return chart
+    charts = {}
+    chart_template = chart
+    for color_option in color_options:
+        charts[color_option] = {}
+        color_vals = sorted(color_options[color_option])
+        color_val_map = {val: i for i, val in enumerate(color_vals)}
+        for shape_option in shape_options:
+            chart = dict(chart_template)
+            shape_vals = sorted(shape_options[shape_option])
+            shape_val_map = {val: i for i, val in enumerate(shape_vals)}
+
+            chart["subtitles"] = chart.get("subtitles", []) + [{
+                    "text": "Colored by %s; Shapes represent %s" % (color_option, shape_option),
+                    "fontSize": 14
+                }]
+            all_ds = []
+            for i, color_val in enumerate(color_vals):
+                for j, shape_val in enumerate(shape_vals):
+                    indices = set(color_options[color_option][color_val]) & set(shape_options[shape_option][shape_val])
+                    ds = {
+                        "type": "scatter",
+                        "markerType": get_shape(shape_val_map[shape_val]),
+                        "markerColor": get_color(color_val_map[color_val]),
+                        "dataPoints": [dp for z, dp in enumerate(dps) if z in indices],
+                    }
+                    all_ds.append(ds)
+            chart["data"] = all_ds
+            charts[color_option][shape_option] = chart
+    return charts
 
 if __name__ == "__main__":
     convert("data/meta.json")
