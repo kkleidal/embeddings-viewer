bfd9cc0411cde78bfbde6316b854d47d0b1ab63f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 08:47:31 +0000

    Adding tarfile member sanitization to extractall()

--- embeddings_viewer.py
+++ embeddings_viewer.py
@@ -38,7 +38,26 @@ class EmbeddingsFlaskApp:
             name = None
         print("Extracting to: %s" % self.tempdir)
         with tarfile.open(name=name, fileobj=fileobj, mode="r:gz") as tar:
-            tar.extractall(self.tempdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, self.tempdir)
         self.entered = True
 
         app = Flask(__name__, static_url_path='')
